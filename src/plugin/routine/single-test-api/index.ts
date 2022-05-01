2fe64d1ee8ad5da3302e4d9aa490f7096daa9401
arily <arily@arily.moe>
2022-05-01 20:51:09 +0900

    add query test

--- src/plugin/routine/single-test-api/index.ts
+++ src/plugin/routine/single-test-api/index.ts
@@ -9,68 +9,45 @@ export interface APIOptions {
 }
 export default function cliSingleTestPlugin (ctx: USBC, options: APIOptions = {}) {
   const server = fastify(options.fastify)
-  server.listen(options.port || 4532, options.host)
+
+  const handler = async (req: { query: { name: string }; params: { name: string } }) => {
+    const name = req.query.name || req.params.name
+    const fakeUser: UserHoldingNames = {
+      isDatabase: false,
+      name,
+      rejected: false,
+      checkResult: [],
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      approve () {},
+      reject (reason) {
+        fakeUser.rejected = true
+        this.checkResult.push(reason)
+      }
+    }
+    await ctx.check(fakeUser)
+    return {
+      ...fakeUser,
+      isDatabase: undefined
+    }
+  }
+
   ctx.useApplication(function TestAPI (ctx: USBC) {
+    server.listen(options.port || 4532, options.host)
     server.get<{
+      Querystring: {
+        name: string
+      },
       Params: {
         name: string
       },
-    }>(options.prefix || '/test' + '/:name', async (req) => {
-      const name = req.params.name
-      const fakeUser: UserHoldingNames = {
-        isDatabase: false,
-        name,
-        rejected: false,
-        checkResult: [],
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        approve () {},
-        reject (reason) {
-          fakeUser.rejected = true
-          this.checkResult.push(reason)
-        }
-      }
-      await ctx.check(fakeUser)
-      return {
-        ...fakeUser,
-        isDatabase: undefined
-
-      }
-    })
+    }>(options.prefix || '/test', handler)
+    server.get<{
+      Querystring: {
+        name: string
+      },
+      Params: {
+        name: string
+      },
+    }>(options.prefix || '/test' + '/:name', handler)
   })
-  // ctx.useApplication(async function LoopTest () {
-  //   while (true) {
-  //     const { name }: { name: string } = await prompts({
-  //       type: 'text',
-  //       name: 'name',
-  //       message: 'try name:'
-  //     })
-
-  //     await ctx.check(fakeUser)
-  //     // console.log()
-  //     if (!fakeUser.rejected) {
-  //       console.log(
-  //         chalk.black.bgCyan(name),
-  //         ': ',
-  //         chalk.green('looks good!')
-  //       )
-  //     } else {
-  //       console.log(
-  //         [chalk.black.bgRedBright(name), ': ',
-  //           chalk.yellow('rejected.'), '\n',
-  //           chalk.bold('reason:'), '\n',
-  //           chalk.gray(fakeUser.rejectReason.join(', \n')), '\n',
-  //           chalk.bold('marked rejection(s):'), '\n',
-  //           fakeUser.checkResult.name.map(({ index, length, positive }) => {
-  //             const before = name.slice(0, index)
-  //             const positivePart = name.slice(index, index + length)
-  //             const after = name.slice(index + length)
-  //             return [
-  //               before + chalk.bgCyanBright.black(positivePart) + after
-  //               // ' '.repeat(index) + '^' + '~'.repeat(length)
-  //             ].join('\n')
-  //           }).join('\n')
-  //         ].join(''))
-  //     }
-  //   }
-  // })
 }
