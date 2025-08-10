91f867378dd87a01734cc25810d22680463c84f3
arily <arily@arily.moe>
2025-08-11 01:21:50 +0800

    ignore case

--- src/plugin/checker/whitelist/index.ts
+++ src/plugin/checker/whitelist/index.ts
@@ -3,17 +3,22 @@ import { DatabaseUserHoldingNames } from 'types/source'
 
 export default function createWhitelist (ctx: USBC, options: string[]) {
   options = options.map(opt => opt.toLowerCase())
-  ctx.useModifier((user) => {
+  ctx.useChecker((user) => {
     if (!user.checkResult.length) {
       return
     }
 
     user.checkResult = user.checkResult.filter(rej => {
       const name = 'field' in rej && rej.field === 'name' ? user.name : (user as DatabaseUserHoldingNames).safeName
-      const before = name.slice(0, rej.index + rej.length)
-      const after = name.slice(rej.index)
+      const before = name.slice(0, rej.index + rej.length).toLowerCase()
+      const after = name.slice(rej.index).toLowerCase()
 
-      return options.every(opt => !before.toLowerCase().endsWith(opt) && !after.toLowerCase().startsWith(opt))
+      return options.every(opt => {
+        opt = opt.toLowerCase()
+        return !before.endsWith(opt) &&
+        !after.startsWith(opt) &&
+        rej.positive !== opt
+      })
     })
 
     if (!user.checkResult.length) {
