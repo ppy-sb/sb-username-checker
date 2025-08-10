c7a37839f62a7120b40f1385395b1ca3ee175680
arily <arily@arily.moe>
2025-08-11 01:22:10 +0800

    fix miscalculated index

--- src/plugin/checker/forbidden-words/utils/match.ts
+++ src/plugin/checker/forbidden-words/utils/match.ts
@@ -1,6 +1,5 @@
 export function getAllIndexes (test: string, val: string, ignoreCase = true, spaced = false): number[] {
   const indexes: number[] = []
-  let i = -1
 
   if (ignoreCase) {
     test = test.toLowerCase()
@@ -9,9 +8,13 @@ export function getAllIndexes (test: string, val: string, ignoreCase = true, spa
 
   if (spaced) {
     if (test === val) return [0]
-    return getAllIndexes(test, val + ' ', ignoreCase).concat(getAllIndexes(test, ' ' + val, ignoreCase))
+    return getAllIndexes(test, val + ' ', ignoreCase)
+      .concat(
+        getAllIndexes(test, ' ' + val, ignoreCase).map(index => index + 1)
+      )
   }
 
+  let i = -1
   while ((i = test.indexOf(val, i + 1)) !== -1) {
     indexes.push(i)
   }
